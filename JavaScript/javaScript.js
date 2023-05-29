bee7cf8640027ffe6006c09d334db3b0a6dbc67e
Luana Tiuma <117609075+luanatiuma@users.noreply.github.com>
2023-05-29 22:41:47 +0000

    Adicionando a pasta de JS

---
+++ JavaScript/javaScript.js
@@ -0,0 +1,49 @@
+
+//Estudo de If/Else
+var i = prompt("Escreva um número")
+if (i > 10){
+    prompt("Este número é maior que 10!")
+}
+else {
+    prompt("Este número é menor que 10!")
+}
+
+// //Estudo de While
+var j = prompt("Escreva um número")
+while (j<10) {
+    j++
+    document.write(j, "\n")
+}
+
+//Estudo de For
+var k = prompt("Multiplicando seu número por 1, 2 e 3")
+for (num = 1; num <= 3; num++) {
+    prompt(num*k)
+}
+
+//Estudo de Switch/Case
+var digit = prompt ("Escreva um número entre 1 e 4")
+
+switch(digit) {
+    case '1':
+        prompt("Este é um número primo")
+        break
+    case '2':
+        prompt("Este é um número primo e par")
+        break
+    case '3':
+        prompt("Este é um número primo")
+        break
+    case '4':
+        prompt("Este é um número par e não é primo")
+        break
+    default:
+        prompt("Este não é um número entre 1 e 4")
+}
+
+//Estudo de Funções
+function escreva(){
+    prompt("Hello!")
+}
+escreva()
+
